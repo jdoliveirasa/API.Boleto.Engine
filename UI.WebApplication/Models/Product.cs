4ea41b0467035596850f192f7e7f695a794cad91
Jonathan <jjnatanx@gmail.com>
2019-01-03 21:58:07 -0800

    crud dev

---
+++ UI.WebApplication/Models/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UI.WebApplication.Models
+{
+    public class Product
+    {
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public double Price { get; set; }
+
+        public int Quantity { get; set; }
+
+        public bool Status { get; set; }
+
+        public string Photo { get; set; }
+
+    }
+}
